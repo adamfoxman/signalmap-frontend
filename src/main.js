b40d361021e4ebab2784708c8490802a0845492b
Adam Lisowski <48516273+adamfoxman@users.noreply.github.com>
2022-01-04 19:07:53 +0100

    Added Vuex

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
@@ -7,12 +8,28 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
+Vue.use(Vuex);
 Vue.component('l-map', LMap);
 Vue.component('l-tile-layer', LTileLayer);
 Vue.component('l-marker', LMarker);
+
+const transmitterStore = new Vuex.Store({
+  state: {
+    transmitters: []
+  },
+  mutations: {
+    addTransmitter(state, transmitter) {
+      state.transmitters.push(transmitter);
+    },
+    removeTransmitter(state, transmitter) {
+      state.transmitters.splice(state.transmitters.indexOf(transmitter), 1);
+    }
+  }
+});
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  store: transmitterStore
+}).$mount('#app');
