13fba46e29c06a1bdc210cfbc17a4e282ec9e90a
Adam Lisowski <48516273+adamfoxman@users.noreply.github.com>
2022-01-24 11:47:33 +0100

    Added screenshots for Help modal

--- src/components/help/HelpButton.vue
+++ src/components/help/HelpButton.vue
@@ -22,6 +22,7 @@
             indicators
             img-width="800"
             img-height="500"
+            style="text-shadow: 1px 1px 2px #333;"
         >
           <b-carousel-slide
               v-for="(slide, index) in slides"
@@ -29,6 +30,7 @@
               :img-src="slide.img"
               :caption="slide.title"
               :text="slide.desc"
+
           />
         </b-carousel>
       </div>
@@ -48,27 +50,27 @@ export default {
       isShowingHelp: false,
       slides: [
         {
-          img: "https://i.imgur.com/XQQZQZL.png",
+          img: "https://i.imgur.com/dzdFxww.png",
           title: "Welcome to SignalMap!",
           desc: "This is a tool for displaying coverage simulations of FM radio, DAB/DAB+ and TV transmitters around the world. Transmitters database is kindly provided by FMList. Coverage simulations were calculated using SPLAT! tool.\nRemember, that presented data is not real, but a simulation of the real situation.\nIt is not meant to be used for any serious purpose."
         },
         {
-          img: "https://i.imgur.com/XQQZQZL.png",
+          img: "https://i.imgur.com/iyEaXAx.png",
           title: "Search the database",
           desc: "Click the search button and enter the band of the transmitter (FM/DAB/TV) you are looking for and country. You can also search by ERP power, frequency, location, station name and antenna polarisation."
         },
         {
-          img: "https://i.imgur.com/XQQZQZL.png",
+          img: "https://i.imgur.com/e6RFb2g.png",
           title: "Choose transmitters",
           desc: "Click on particular transmitter you want to check and then click the \"Add selected\" button. You can also add all found transmitters at once by clicking \"Add all\""
         },
         {
-          img: "https://i.imgur.com/XQQZQZL.png",
+          img: "https://i.imgur.com/kkaCLwI.png",
           title: "Check the coverage",
           desc: "Click the \"Show transmitters\" button to see the transmitters you have selected. Click the \"Show\" button to see it's coverage simulation on the map. You can also hide displayed transmitters by clicking the \"Hide\" button and delete them from this list by clicking the \"Delete\" button."
         },
         {
-          img: "https://i.imgur.com/XQQZQZL.png",
+          img: "https://i.imgur.com/5G1I3fs.png",
           title: "Display more data",
           desc: "Click on particular transmitter you want to check and the popup with more data will appear."
         }
